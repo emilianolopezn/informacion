bcb526bc0d8dcdf6435c1c8b3e3888eb05da2eb3
Ulsa <ulsa@ulsa.com>
2019-10-01 13:03:42 -0700

    Initial Commit

---
+++ informacion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  informacion
+//
+//  Created by MaestroD1 on 10/1/19.
+//  Copyright © 2019 MaestroD1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ informacionTests/informacionTests.swift
@@ -0,0 +1,34 @@
+//
+//  informacionTests.swift
+//  informacionTests
+//
+//  Created by MaestroD1 on 10/1/19.
+//  Copyright © 2019 MaestroD1. All rights reserved.
+//
+
+import XCTest
+@testable import informacion
+
+class informacionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
